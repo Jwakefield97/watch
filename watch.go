0f0bfbd05975fce17a2a8f58dc528599d5b5138e
jwakefield97 <jacobstevenwakefield@gmail.com>
2019-05-21 15:58:14 -0500

    init

---
+++ watch.go
@@ -0,0 +1,57 @@
+package main 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+	"os/exec"
+	"bytes"
+	"log"
+	"regexp"
+)
+
+func WatchFiles(dir string, regexStr string, script string) {
+	files := make(map[string]time.Time)
+	isFirst := true
+	regex, err := regexp.Compile(regexStr)
+	if err != nil {
+		log.Fatal(err)  
+	}
+	for {
+		err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+			matched := regex.MatchString(path)
+			
+			if matched && !info.IsDir() && info.ModTime() != files[path] {
+
+				files[path] = info.ModTime()
+				if(!isFirst) {
+
+					fmt.Println(path)
+					cmd := exec.Command("bash", script)                                                                                                                                                          
+					var out bytes.Buffer                                                                                                                                                                                   
+					cmd.Stdout = &out                                                                                                                                                                                    
+					err := cmd.Run()                                                                                                                                                                                     
+					if err != nil  {                                                                                                                                                                                            
+							log.Fatal(err)                                                                                                                                                                               
+					}                                                                                                                                                                                                          
+					fmt.Printf(out.String())
+
+				}
+			}
+			return nil
+		})
+		if err != nil {
+			fmt.Println("error occured while walking directory")
+		}
+		isFirst = false
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	fmt.Println("Usage: ./watch <directory> <regex_to_match_files> <script_to_run_if_file_matched>")
+	dir := os.Args[1]
+	regex := os.Args[2]
+	script := os.Args[3]
+	WatchFiles(dir,regex,script)
+}
